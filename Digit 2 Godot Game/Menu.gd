4eb03b9ce6d1a6679061f6578dc08edcecda4e60
Vinz-A <vinz_abu@student.asdah.school.nz>
2023-08-10 12:52:43 +1200

    Added the controls and Credits
    Signed-off-by: Vinz-A <vinz_abu@student.asdah.school.nz>

--- Digit 2 Godot Game/Menu.gd
+++ Digit 2 Godot Game/Menu.gd
@@ -4,7 +4,15 @@ extends Control
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://level.tscn")
-
+	
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_controls_pressed():
+	get_tree().change_scene_to_file("res://control_menu.tscn")
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://credits_.tscn")
