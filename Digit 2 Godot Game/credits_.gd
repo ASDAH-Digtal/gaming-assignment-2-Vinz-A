f041b69770474661cb875616428ab56bb3d25132
Vinz <86935359+Vinz-A@users.noreply.github.com>
2023-10-11 13:14:43 +1300

    Removed Menu and Game tabs

--- Digit 2 Godot Game/credits_.gd
+++ Digit 2 Godot Game/credits_.gd
@@ -4,3 +4,5 @@ extends Control
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menu.tscn")
+
+
---
+++ Digit 2 Godot Game/pause_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+#Abandoned this script
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
